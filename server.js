a32f1da99f0ce38ac7e3bb841e05c212c261ae8b
Bogdan Karlenko <bogdan@77labs.com>
2018-10-24 15:55:47 +0300

    add authorization, registration, restructured

--- server.js
+++ server.js
@@ -1,41 +1,48 @@
 'use strict';
+
 const express = require('express');
+const app = express();
+const router = express.Router();
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const mongoose = require('mongoose');
+
 const restify = require('express-restify-mongoose');
-const app = express();
-const router = express.Router();
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-mongoose.connect('mongodb://ebhealth-prod:gVurhNx6Bz1prFbi@cluster0-shard-00-00-4ywtq.mongodb.net:27017,cluster0-shard-00-01-4ywtq.mongodb.net:27017,cluster0-shard-00-02-4ywtq.mongodb.net:27017/ebhealth?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 
 require('./models/Provider');
-const Provider = mongoose.model('Provider');
+require('./models/User');
 
-restify.serve(router, Provider);
+require('./config/passport');
 
+restify.serve(router, mongoose.model('Provider'), { preMiddleware: passport.authenticate('jwt', { session: false })});
+
+app.use(passport.initialize());
 app.use(router);
 
-router.get('/', function (req, res) {
-    res.send('ebhealth-backend is up')
+router.get('/', (req, res) => {
+    res.send('ebhealth-backend API')
 })
 
-router.get('/api/v1/devices', (req, res) => {
-    Provider.distinct('devices', (err, result)=>{
-        if (err) res.statusCode(500);
-        res.send(result.sort());
-    })
-})
+app.use('/api/v1/devices', passport.authenticate('jwt', { session: false }), require('./routes/devices'))
+app.use('/api/v1/login', require('./routes/login'));
+app.use('/api/v1/register', require('./routes/register'));
+app.use('/api/v1/user', passport.authenticate('jwt', { session: false }), require('./routes/user'));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('-------------------------')
     console.log('listening on port', process.env.PORT || 3000, '\n');
-    console.log('Press Ctrl+C to exit');
     console.log('-------------------------');
 });
